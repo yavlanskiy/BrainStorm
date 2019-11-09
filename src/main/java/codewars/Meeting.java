d0a5cbb4ccd474dd5bbdecd859d8981b8da5e4c7
yavlanskiy <yavlanskiy@gmail.com>
2019-11-09 21:29:38 +0200

    Meeting

--- src/main/java/codewars/Meeting.java
+++ src/main/java/codewars/Meeting.java
@@ -1,7 +1,22 @@
 package codewars;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+ */
 public class Meeting {
-    public static String meeting(String s) {
-        return null;
+    public static String meeting(String rawString) {
+        return Arrays
+                .stream(rawString.toUpperCase().split(";"))
+                .map(s -> s.replaceAll("(\\w+):(\\w+)","($2, $1)")).sorted()
+                .collect(Collectors.joining(""));
+    }
+
+    public static void main(String[] args) {
+         String s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+
+        System.out.println(meeting(s));
     }
 }
