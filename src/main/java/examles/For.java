0f97f5d313c1f38a61fc9b76262fc3c6975908c8
Maxim Yavlanskiy <yavlanskiy@gmail.com>
2019-11-11 10:12:11 +0200

    ***

---
+++ src/main/java/examles/For.java
@@ -0,0 +1,12 @@
+package examles;
+
+import java.util.ArrayList;
+
+public class For {
+    private ArrayList<String> tabs;
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+        }
+    }
+}
