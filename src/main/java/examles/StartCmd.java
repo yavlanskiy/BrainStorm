a4d9b45eb204f86414078dfdc04f52f8fe35b59a
Maksim Yavlyanskiy <max_yav@csltd.com.ua>
2021-12-01 10:22:28 +0200

    Мусор

--- src/main/java/examles/StartCmd.java
+++ src/main/java/examles/StartCmd.java
@@ -13,13 +13,35 @@ public class StartCmd{
         valuesMap.put("1", "1");
         valuesMap.put("2", "2");
         valuesMap.put("3", "3");
-        valuesMap.put("4", "4");
+        //valuesMap.put("4", "4");
 
 
+        // 1. Установку константы (если есть)
+
+        // 2.
         String templateString = "java -jar nbuxml-1.0.jar -a ${1} -s creator -u creator -p c67 -rd ${2} -loglevel INFO -ft ${3} -outpath ${4}";
         //String templateString = "The ${animal} jumped over the ${target}.";
+
         StringSubstitutor sub = new StringSubstitutor(valuesMap);
         String resolvedString = sub.replace(templateString);
         System.out.println(resolvedString);
     }
+
+
+
 }
+
+
+//[09:35] Igor Maschikevich
+//        Привіт.
+//        java -jar D:\SVN\Automation\Development\GIT\xml_scripts\build\release\nbuxml.jar -a mizar_Z3 -s CREATOR -u CREATOR -p c67 -rd 01.10.2036 -loglevel INFO -ft FI5X -outpath D:\SVN\Automation\Development\GIT\xml_scripts\build\CurrentFiles\3.48.0.0
+//
+//        [09:35] Igor Maschikevich
+//        Приклад строки зроблений на базі теста 10437
+//        -rd 01.10.2036 це параметер теста
+//        -ft FI5X це параметер теста
+//        -outpath D:\\CurrentFiles шлях куди буде згенерований xml файл
+//        java -jar D:\\nbuxml.jar старт для nbuxml.jar
+//        -s mizar_z3 база на якій буде запускатися nbuxml.jar
+//        -loglevel INFO це лог nbuxml.jar, від нього мож на віказатися
+
