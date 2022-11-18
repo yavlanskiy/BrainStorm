0eb0fa45817e48d8e0f57ca87fd48d6346f11ffe
Maksim Yavlyanskiy <max_yav@csltd.com.ua>
2022-11-18 18:55:34 +0200

    logger Log4j2

---
+++ src/main/java/Log4j2/Main.java
@@ -0,0 +1,16 @@
+package Log4j2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
+        logger.fatal("This is a fatal message");
+    }
+}
