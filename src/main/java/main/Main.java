0ed249eec720323f5021d8673625d538de26ed7c
Maksim Yavlyanskiy <max_yav@csltd.com.ua>
2023-02-20 17:41:28 +0200

    test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,11 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean screeen =  false;
-        boolean drower = true;
-
-        if (screeen & drower) {
-            System.out.println("hello");
-        }
+        System.out.print("Hello");
     }
 }
